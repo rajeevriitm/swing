baf6707e9156042df6c5c270ad57d0e8e297e725
rajeev <rajeevriitm@gmail.com>
2017-01-31 16:46:50 +0530

    update swing.js

--- dist/index.js
+++ dist/index.js
@@ -5,28 +5,20 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Stack = exports.Direction = exports.Card = undefined;
 
-var _card = require('./card');
+var _Card = require('./Card');
 
-var _card2 = _interopRequireDefault(_card);
+var _Card2 = _interopRequireDefault(_Card);
 
-var _direction = require('./direction.enum');
+var _Direction = require('./Direction');
 
-var _direction2 = _interopRequireDefault(_direction);
+var _Direction2 = _interopRequireDefault(_Direction);
 
-var _stack = require('./stack');
+var _Stack = require('./Stack');
 
-var _stack2 = _interopRequireDefault(_stack);
+var _Stack2 = _interopRequireDefault(_Stack);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-global.gajus = global.gajus || {};
-
-global.gajus.Swing = {
-  Card: _card2.default,
-  Direction: _direction2.default,
-  Stack: _stack2.default
-};
-
-exports.Card = _card2.default;
-exports.Direction = _direction2.default;
-exports.Stack = _stack2.default;
+exports.Card = _Card2.default;
+exports.Direction = _Direction2.default;
+exports.Stack = _Stack2.default;
